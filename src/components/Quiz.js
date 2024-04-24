b37677e267dfc496b441e778a0b70b3655c137cc
Lizzie Coady <lizziecoady@hotmail.co.uk>
2024-04-24 12:49:13 +0100

    Use object in useState

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,10 +2,16 @@ import { useState } from "react";
 import Question from "./Question";
 
 const Quiz = () => {
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0) // useStae uses: [state, setState] = useState(initialState)  
+  const [state, setState] = useState({
+    currentQuestionIndex: 0,
+    questions: []
+  }) // useStae uses: [state, setState] = useState(initialState)  
   const testClick = () => {
     console.log('clicckkk');
-    setCurrentQuestionIndex(currentQuestionIndex + 1) // This will update the state, and therefore re-renders our component e.g. it initially renders as 0, then re-renders as 1, then 2 etc.
+    setState({
+      ...state,
+      currentQuestionIndex: state.currentQuestionIndex +1
+    }) // This will update the state, and therefore re-renders our component e.g. it initially renders as 0, then re-renders as 1, then 2 etc.
   }
 
   return (
@@ -15,7 +21,9 @@ const Quiz = () => {
           Question 1/8
         </div>
         <Question />
-        <div className="next-button" onClick={testClick}>Next Question {currentQuestionIndex}</div>
+        <div className="next-button" onClick={testClick}>
+          Next Question {state.currentQuestionIndex}
+        </div>
       </div>
     </div>
   );
